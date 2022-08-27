e449743f364c65589e36651e6e4738f03f5ed5ea
Fazal Haroon <36573782+Fazal-haroon@users.noreply.github.com>
2022-08-27 14:54:22 +0400

    deleted

--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    //all crud database methods
-}
